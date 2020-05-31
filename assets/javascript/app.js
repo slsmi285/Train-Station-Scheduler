f0e99a4cb886ede25fb4575b6a646007cef42a30
Sandra Smith <toosandra@gmail.com>
2020-05-31 14:30:28 -0400

    Added bootstrap jumbotron, card header to strictor control, and table form for the user input of information.

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+    window.addEventListener('load', function() {
+        //for all the forms, applying Bootstrap styles
+        var forms = document.getElementsByClassName('needs-validation');
+    })
+})
