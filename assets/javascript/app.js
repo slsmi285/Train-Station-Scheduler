b7da01047d6e0545b4786a965925ea53802b2d25
Sandra Smith <toosandra@gmail.com>
2020-06-02 16:52:50 -0400

    reviewed github and noticed all my commits are missing.  only showing 3 commits, when there were more.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -80,5 +80,77 @@ var tFrequency = parseInt($("#train-frequency").val().trim());
         $("#train-first-time").attr("class", "form-control is-invalid");
         $("#invalid-time").text("Please enter a valid time");
 
-        return
+//return;
     };
+
+    $("#train-first-time").attr("class", "form-control");
+        $("#invalid-time").text("");
+
+        //Creates local object for holding train data
+        var newTrain = {
+            name: tName,
+            destination: tDestination,
+            firstTime: tFirstTime.format("HH:mm"),
+            frequency: tFrequency
+        };
+        $("#train-first-time").attr("class", "form-group");
+
+        $("#helpBlock").text("");
+
+        //Code for pushing train data to Firebase
+        database.ref().push(newTrain);
+
+        console.log(newTrain.name);
+        console.log(newTrain.destination);
+        console.log(newTrain.firstTime);
+        console.log(newTrain.frequency);
+
+        //Clear all of text-boxes
+        $("#train-name").val("");
+        $("#train-destination").val("");
+        $("#train-first-time").val("");
+        $("#train-frequency").val("");
+    
+
+    //Firebase watcher + initial loader
+    database.ref().on("child_added", function(childSnapshot) {
+        var tName = (childSnapshot.val().name);
+        var tDestination = (childSnapshot.val().destination);
+        var tFirstTime = (childSnapshot.val().firstTime);
+        var tFrequency = (childSnapshot.val().frequency);
+
+        var convertedTime = moment(tFirstTime, "HH:mm").subtract(1, "years");
+        console.log(convertedTime);
+
+        //Current Time
+        var currentTime = moment();
+
+        //Difference between the times
+        var diffTime = moment().diff(moment(convertedTime), "minutes");
+        console.log("Difference in time: " + diffTime);
+
+        //Time apart
+        var tRemainder = diffTime % tFrequency;
+        console.log(tRemainder);
+
+        //Minutes Until Train
+        var minutesAway = tFrequency - tRemainder;
+        console.log("Minutes until train: " + minutesAway);
+
+        //Next Train
+        var nextArrival = moment().add(minutesAway, "minutes");
+        console.log("Arrival time: " + moment(nextArrival).format("HH:mm"));
+
+        //Create the new row
+        var newRow = $("<tr>").append(
+            $("<td>").text(tName),
+            $("<td>").text(tDestination),
+            $("<td>").text(tFrequency),
+            $("<td>").text(nextArrival.format("HH:mm")),
+            $("<td>").text(minutesAway),
+        );
+
+        //Append the new row to the table
+        $("#full-table").append(newRow);
+    })
+    
